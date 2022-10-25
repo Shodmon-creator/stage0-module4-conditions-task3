ca73225051062d5c3d02b2148397d273a5fa111e
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-25 23:56:14 +0500

    7 tasks have been done

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        int sum=0;
+        for (int i =1; i < Math.sqrt(first); i++) {
+            if (first%i==0){
+                sum=+i;
+            }
+        } if (sum==first){
+            System.out.println("Aliquot");
+        }else {
+            System.out.println("Not aliquot");
+        }
     }
 }
