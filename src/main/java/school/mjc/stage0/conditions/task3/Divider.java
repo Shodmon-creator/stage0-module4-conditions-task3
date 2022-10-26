996dace8853d48fc4ff4f8c59ecd151465fd5ace
Shodmon <mamayusupovshodmon2003@gmail.com>
2022-10-26 11:33:36 +0500

    some mistakes have been corrected

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
-        if (number%5==0||number%7==0){
-            System.out.println("Dividable");
-        }else if (number/5==0&&number/11==0){
-            System.out.println("cannot divide by zero");
+
+        int five=5;
+        int eleven=11;
+        if (five!=0||eleven!=0){
+            if (number%5==0||number%7==0){
+                System.out.println("Dividable");
+            }else {
+                System.out.println("cannot divide by zero");
+            }
         }else {
             System.out.println("Non-dividable");
         }
